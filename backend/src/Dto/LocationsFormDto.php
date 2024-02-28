201e8b0c9fc0338f5e7c12c874c85674471edca0
Tiago Pinto <tigmmp@hotmail.com>
2024-02-28 21:43:51 +0000

    [HIKING-44]/Initialize DTOs

--- backend/src/Dto/LocationsFormDto.php
+++ backend/src/Dto/LocationsFormDto.php
@@ -6,27 +6,21 @@ use App\Utils\DtoUtils;
 
 class LocationsFormDto
 {
-    private ?int $districtId = null;
-    private ?string $districtName = null;
-    private ?int $countyId = null;
-    private ?string $countyName = null;
-    private ?int $locationId = null;
-    private ?string $locationName = null;
+    private ?int $districtId;
+    private ?string $districtName;
+    private ?int $countyId;
+    private ?string $countyName;
+    private ?int $locationId;
+    private ?string $locationName;
 
     public function __construct(array $parameters, bool $isStart = false)
     {
         $this->districtId = DtoUtils::getInt($parameters, $isStart ? "startDistrictId" : "districtId");
-        if (!$this->districtId) {
-            $this->districtName = DtoUtils::getString($parameters, $isStart ? "startDistrictId" : "districtId");
-        }
+        $this->districtName = !$this->districtId ? DtoUtils::getString($parameters, $isStart ? "startDistrictId" : "districtId") : null;
         $this->countyId = DtoUtils::getInt($parameters, $isStart ? "startCountyId" : "countyId");
-        if (!$this->countyId) {
-            $this->countyName = DtoUtils::getString($parameters, $isStart ? "startCountyId" : "countyId");
-        }
+        $this->countyName = !$this->countyId ? DtoUtils::getString($parameters, $isStart ? "startCountyId" : "countyId") : null;
         $this->locationId = DtoUtils::getInt($parameters, $isStart ? "startLocationId" : "locationId");
-        if (!$this->locationId) {
-            $this->locationName = DtoUtils::getString($parameters, $isStart ? "startLocationId" : "locationId");
-        }
+        $this->locationName = !$this->locationId ? DtoUtils::getString($parameters, $isStart ? "startLocationId" : "locationId") : null;
     }
 
     public function getDistrictId(): ?int
