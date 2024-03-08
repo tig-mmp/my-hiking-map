d56369938f8507e1b17b3c1ef470eb3739ea5fd9
Tiago Pinto <tigmmp@hotmail.com>
2024-03-08 22:40:16 +0000

    [HIKING-52]/Create get moita tracks API

---
+++ backend/src/Controller/TracksMoitaController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TrackRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TracksMoitaController extends AbstractController
+{
+    #[Route("/api/tracks/moita", name: "list-tracks-moita", methods: ["GET"])]
+    public function listTracks(TrackRepository $trackRep): JsonResponse
+    {
+        $tracks = $trackRep->findBy(["isMoita" => true]);
+        $tracksSerialized = [];
+        foreach ($tracks as $track) {
+            $tracksSerialized[] = $track->serializeMap();
+        }
+        return $this->json(["data" => $tracksSerialized]);
+    }
+}
