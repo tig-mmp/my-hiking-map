7bd0acd58b22f5e06cc200bab6cdac3783e85a6e
Tiago Pinto <tigmmp@hotmail.com>
2024-03-09 13:41:07 +0000

    [HIKING-58]/Create a loading screen

---
+++ moita/src/components/Loading.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="loadingBox">
+        <img src="/loading.svg" class="loading" alt="loading">
+    </div>
+</template>
+
+<style scoped>
+@keyframes fadeInLeft {
+    0% {
+        opacity: 0;
+        transform: translateX(-100%);
+    }
+
+    50% {
+        opacity: 1;
+        transform: translateX(0%);
+    }
+
+    100% {
+        opacity: 0;
+        transform: translateX(100%);
+    }
+}
+
+div.loadingBox {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+img.loading {
+    height: 30vh;
+    animation: fadeInLeft 3s linear infinite;
+}
+</style>
