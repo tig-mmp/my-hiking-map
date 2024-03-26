47fbc7c4cd23b59755da9ffb64b6aa0c52bd647c
Tiago Pinto <tigmmp@hotmail.com>
2024-03-26 23:19:17 +0000

    [HIKING-66]/Shuffle landmarks

--- moita/src/components/ListLandmarks.vue
+++ moita/src/components/ListLandmarks.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
 import { landmarkMapDataType, type LandmarkMap } from "@/models/landmark/map";
-import { defineAsyncComponent, onMounted } from "vue";
+import { Ref, defineAsyncComponent, onMounted, ref } from "vue";
 import useApiRoutes from "@/composables/api/useApiRoutes";
 import useApiGet from "@/composables/api/useApiGet";
 import useEnvironment from "@/composables/useEnvironment";
 
 const Loading = defineAsyncComponent(() => import("@/components/Loading.vue"));
+const LandmarkPage = defineAsyncComponent(() => import("@/components/LandmarkPage.vue"));
 
 const { landmarksApi } = useApiRoutes();
 const { BASE } = useEnvironment();
 
+const selectedLandmark: Ref<LandmarkMap | null> = ref(null);
+
 const { load: load, data: landmarks, isLoading } = useApiGet<LandmarkMap[]>([]);
 const getData = () => load(landmarksApi, { dataType: landmarkMapDataType })
-  .then(() => landmarks.value = landmarks.value.filter((l) => !!l.file));
+  .then(() => {
+    landmarks.value = landmarks.value.filter((l) => !!l.file);
+    landmarks.value.sort(() => Math.random() - 0.5);
+  });
 
 onMounted(() => getData());
 
@@ -21,10 +27,13 @@ onMounted(() => getData());
 <template>
   <Loading v-if="isLoading" />
   <div class="grid">
-    <div v-for="landmark in landmarks" :key="landmark.id" class="wrapper padded-container">
+    <div v-for="landmark in landmarks" :key="landmark.id" class="wrapper padded-container"
+      @click="showLandmark(landmark)">
       <img :src="`${BASE}${landmark.file?.url}`" :alt="landmark.file?.name" class="img centered" />
     </div>
   </div>
+
+  <LandmarkPage v-if="selectedLandmark" :landmark="selectedLandmark" @close="showLandmark(null)" />
 </template>
 
 <style>
